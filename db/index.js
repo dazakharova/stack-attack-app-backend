0b5a8dc791acdc2eda7d6a6af3a33d2111ecad7d
Daria Zakharova <t3zada00@students.oamk.fi>
2024-04-02 16:11:14 +0300

    Add PostgreSQL database connection setup

---
+++ db/index.js
@@ -0,0 +1,28 @@
+const { Pool } = require('pg')
+
+const query = async (sql, values = []) => {
+    return new Promise (async (resolve, reject) => {
+        try {
+            const pool = openDb()
+            const result = await pool.query(sql, values)
+            resolve(result)
+        } catch (error) {
+            reject(error.message)
+        }
+    })
+}
+
+const openDb = ()=> {
+    const pool = new Pool({
+        user: 'postgres',
+        host: 'localhost',
+        database: 'stack_attack',
+        password: 'root',
+        port: 5432
+    })
+    return pool
+}
+
+module.exports = {
+    query
+}
