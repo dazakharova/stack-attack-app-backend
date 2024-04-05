17387e197de405a01a20f376762d999bf516c642
Daria Zakharova <t3zada00@students.oamk.fi>
2024-04-05 16:23:48 +0300

    Set up CORS for client-side interactions and parse cookies for auth

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const cookieParser = require('cookie-parser');
 const containersRouter = require('./routes/containers')
 const itemsRouter = require('./routes/items')
 const authRouter = require('./routes/auth')
@@ -8,7 +9,11 @@ const authRouter = require('./routes/auth')
 const app = express()
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: 'http://127.0.0.1:5500', // Adjust this to your client's origin
+    credentials: true,
+}));
+app.use(cookieParser());
 app.use('/containers', containersRouter)
 app.use('/items', itemsRouter)
 app.use('/auth', authRouter)
