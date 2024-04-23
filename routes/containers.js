92395037da33e6e57798730212e365f0b6b1424e
Cristofor4ik <xtinabruscaginxxx@gmail.com>
2024-04-23 23:26:10 +0300

    Add handling for updating box color in PUT route for containers

--- routes/containers.js
+++ routes/containers.js
@@ -1,71 +1,95 @@
-const containersRouter = require('express').Router();
-const { query } = require('../db/index.js');
-const { authenticateToken } = require('../middleware/authentication');
+const containersRouter = require("express").Router();
+const { query } = require("../db/index.js");
+const { authenticateToken } = require("../middleware/authentication");
 
 // Create a new container
-containersRouter.post('/', authenticateToken, async (request, response) => {
-    const { name, parent_id } = request.body;
+containersRouter.post("/", authenticateToken, async (request, response) => {
+  const { name, parent_id } = request.body;
 
-    try {
-        let result;
-        if (parent_id) { // If parent_id is provided, use it in the query
-            result = await query(
-                'INSERT INTO containers (name, parent_id, user_id) VALUES ($1, $2, $3) RETURNING *',
-                [name, parent_id, request.userID]
-            );
-        } else { // If parent_id is not provided, exclude it from the query
-            result = await query(
-                'INSERT INTO containers (name, user_id) VALUES ($1, $2) RETURNING *',
-                [name, request.userID]
-            );
-        }
-
-        // Respond with all container records for the user
-        response.status(201).json(result.rows);
-    } catch (error) {
-        console.error(error);
-        response.status(500).json({ error: error })
+  try {
+    let result;
+    if (parent_id) {
+      // If parent_id is provided, use it in the query
+      result = await query(
+        "INSERT INTO containers (name, parent_id, user_id) VALUES ($1, $2, $3) RETURNING *",
+        [name, parent_id, request.userID]
+      );
+    } else {
+      // If parent_id is not provided, exclude it from the query
+      result = await query(
+        "INSERT INTO containers (name, user_id) VALUES ($1, $2) RETURNING *",
+        [name, request.userID]
+      );
     }
-})
+
+    // Respond with all container records for the user
+    response.status(201).json(result.rows);
+  } catch (error) {
+    console.error(error);
+    response.status(500).json({ error: error });
+  }
+});
 
 // Return all containers for user
-containersRouter.get('/', authenticateToken, async(request, response) => {
-    try {
-        const result = await query('SELECT * FROM containers WHERE user_id = $1', [request.userID]);
-        response.status(200).json(result.rows);
-    } catch (error) {
-        console.error(error);
-        response.status(500).json({ error: error });
-    }
-})
+containersRouter.get("/", authenticateToken, async (request, response) => {
+  try {
+    const result = await query("SELECT * FROM containers WHERE user_id = $1", [
+      request.userID,
+    ]);
+    response.status(200).json(result.rows);
+  } catch (error) {
+    console.error(error);
+    response.status(500).json({ error: error });
+  }
+});
 
 // Update a container
-containersRouter.put('/:id', authenticateToken, async(request, response) => {
-    const { id } = request.params;
-    const { name } = request.body;
+containersRouter.put("/:id", authenticateToken, async (request, response) => {
+  const { id } = request.params;
+  const { name, boxcolor } = request.body;
 
-    try {
-        const result = await query('UPDATE containers SET name = $1 WHERE id = $2 RETURNING *', [
-            name, id
-        ]);
-        response.status(200).json(result.rows[0])
-    } catch (error) {
-        console.error(error);
-        response.status(500).json({ error: error })
+  try {
+    if (name) {
+      const result = await query(
+        "UPDATE items SET name = $1 WHERE id = $2 RETURNING *",
+        [name, id]
+      );
+      response.status(200).json(result.rows[0]);
+    } else if (boxcolor) {
+      const result = await query(
+        "UPDATE items SET description = $1 WHERE id = $2 RETURNING *",
+        [boxcolor, id]
+      );
+      response.status(200).json(result.rows[0]);
+    } else {
+      response
+        .status(400)
+        .json({ error: "Neither name nor boxcolor provided for update." });
     }
-})
+  } catch (error) {
+    console.error(error);
+    response.status(500).json({ error: error });
+  }
+});
 
 // Delete a container
-containersRouter.delete('/:id', authenticateToken, async(request, response) => {
+containersRouter.delete(
+  "/:id",
+  authenticateToken,
+  async (request, response) => {
     const { id } = request.params;
 
     try {
-        const result = await query('DELETE FROM containers WHERE id = $1 RETURNING *', [id]);
-        response.status(200).json(result.rows[0]);
-    } catch(error) {
-        console.error(error);
-        response.status(500).json({ error: error });
+      const result = await query(
+        "DELETE FROM containers WHERE id = $1 RETURNING *",
+        [id]
+      );
+      response.status(200).json(result.rows[0]);
+    } catch (error) {
+      console.error(error);
+      response.status(500).json({ error: error });
     }
-})
+  }
+);
 
-module.exports = containersRouter
+module.exports = containersRouter;
