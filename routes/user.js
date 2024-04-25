aeef2b086465fd5394cad074d4138a219b8158a1
Daria Zakharova <t3zada00@students.oamk.fi>
2024-04-25 07:39:51 +0300

    Add users handler for getting profile picture

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,18 @@ const userRouter = require('express').Router();
 const { query } = require('../db/index.js');
 const { authenticateToken } = require('../middleware/authentication');
 
-// Update a prifle picture
+// Get a profile picture
+userRouter.get('/getPicture', authenticateToken, async(request, response) => {
+    try {
+        const result = await query('GET profile_pic WHERE id = $1', [request.userID]);
+        response.status(201).json(result.rows[0]);
+    } catch (error) {
+        console.error(error);
+        response.status(500).json({ error: error });
+    }
+})
+
+// Update a profile picture
 userRouter.put('/updatePicture', authenticateToken, async(request, response) => {
     const { image } = request.body
 
