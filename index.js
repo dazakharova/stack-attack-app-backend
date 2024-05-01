0d58ea0643f20d1e8ecfba50a8f61d056cb1dcb0
Daria Zakharova <t3zada00@students.oamk.fi>
2024-05-01 06:31:05 +0300

    Use environment variable for CORS origin

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express()
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors({
-    origin: 'http://127.0.0.1:5500', // Adjust this to your client's origin
+    origin: process.env.CORS_ORIGIN,
     credentials: true,
 }));
 app.use(cookieParser());
